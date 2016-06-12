6c2df5624aac21cf7504ebd389b3b7bd5e52510d
Pavlo <pmatsibokh@gmail.com>
2016-06-12 18:51:55 +0000

    contact form on php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
@@ -16,11 +15,9 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'pmatsibokh@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$to = 'pmatsibokh@gmail.com';
+$email_subject = "New message form:  $name in your website";
+$email_body = "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+mail($to,$email_subject,$email_body);
 return true;			
 ?>
