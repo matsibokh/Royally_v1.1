8a4e3c21e87e9a9e06a31afeb35661f4bab68486
Pavlo <pmatsibokh@gmail.com>
2016-06-12 19:21:39 +0000

    Initial

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,7 +39,17 @@ $(function() {
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                }
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
             })
         },
         filter: function() {
